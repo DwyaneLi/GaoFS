d376416d43f23009c321cdccff64472dbedb2ddf
dwyaneli <757906626@qq.com>
2023-02-18 14:14:48 +0800

    chunk storage

--- include/daemon/backend/data/file_handle.hpp
+++ include/daemon/backend/data/file_handle.hpp
@@ -5,9 +5,73 @@
 #ifndef GAOFS_FILE_HANDLE_HPP
 #define GAOFS_FILE_HANDLE_HPP
 
-
+#include <daemon/backend/data/data_module.hpp>
 extern "C" {
 #include <unistd.h>
+};
+
+// 文件句柄类封装文件描述符，允许RAII关闭文件描述符。
+class FileHandle {
+
+private:
+    constexpr static const int init_value{-1};
+
+    int fd_{init_value}; // 文件描述符
+    std::string path_{}; // 文件路径
+
+public:
+
+    FileHandle() = default;
+
+    // 构造函数
+    FileHandle(int fd, std::string& path) noexcept : fd_(fd), path_(path) {}
+
+    // 移动构造函数
+    FileHandle(FileHandle&& rhs) = default;
+
+    // 拷贝构造函数
+    FileHandle(const FileHandle& other) = delete;
+
+    FileHandle& operator=(FileHandle&& rhs) = default;
+
+    FileHandle& operator=(const FileHandle& other) = delete;
+
+    // 判断fd是否有效，不是-1就认为有效
+    [[nodiscard]] bool valid() const noexcept {
+        return fd_ != init_value;
+    }
+
+    // 重载bool类型转化 bool a = fh可以认为 bool a = fh.valid();
+    explicit operator bool() const noexcept {
+        return valid();
+    }
+
+
+    // 返回fd_
+    [[nodiscard]] int native() const noexcept {
+        return fd_;
+    }
+
+    // 关闭文件，并把fd_回复默认
+    bool close() noexcept {
+        if(fd_ != init_value) {
+            if(::close(fd_) < 0) {
+                GAOFS_DATA_MOD->log()->warn(
+                        "{}() Failed to close file descriptor '{}' path '{}' errno '{}'",
+                        __func__, fd_, path_, ::strerror(errno)
+                        );
+                return false;
+            }
+            fd_ = init_value;
+            return true;
+        }
+    }
+
+    // 析构函数
+    ~FileHandle() {
+        if(fd_ != init_value)
+            close();
+    }
 };
 
 #endif //GAOFS_FILE_HANDLE_HPP
--- src/daemon/backend/data/chunk_storage.cpp
+++ src/daemon/backend/data/chunk_storage.cpp
@@ -54,6 +54,20 @@ void ChunkStorage::init_chunk_space(const string &file_path) const {
     }
 }
 
+// 删除file对应的存chunk的目录
+void ChunkStorage::destroy_chunk_space(const string &file_path) const {
+    auto chunk_dir = absolute(get_chunks_dir(file_path));
+    try {
+        auto n = fs::remove_all(chunk_dir);
+        log_->debug("{}() Removed '{}' files and directories from '{}'",
+                    __func__, n, chunk_dir);
+    } catch (const fs::filesystem_error& e) {
+        auto err_str = fmt::format("{}() Failed to remove chunk directory. Path: '{}', Error: '{}'",
+                                   __func__, chunk_dir, e.what());
+        throw ChunkStorageException(e.code().value(), err_str);
+    }
+}
+
 ChunkStorage::ChunkStorage(string &path, size_t chunksize) : root_path_(path), chunksize_(chunksize) {
     // 创建logger（日志）实例
     GAOFS_DATA_MOD->log(spdlog::get(GAOFS_DATA_MOD->LOGGER_NAME));
@@ -63,7 +77,7 @@ ChunkStorage::ChunkStorage(string &path, size_t chunksize) : root_path_(path), c
     assert(gaofs::path::is_absolute(root_path_));
 
     // 验证是否对放块的根目录有足够的访问权限
-    if(access(root_path_.c_str(), W_OK | R_OK != 0)) {
+    if(access(root_path_.c_str(), W_OK | R_OK) != 0) {
         auto err_str = fmt::format(
                 "{}() Insufficient permissions to create chunk directories in path '{}'",
                 __func__, root_path_
@@ -72,6 +86,113 @@ ChunkStorage::ChunkStorage(string &path, size_t chunksize) : root_path_(path), c
         throw ChunkStorageException(EPERM, err_str);
     }
     log_->debug("{}() Chunk storage initialized with path: '{}'", __func__, root_path_);
+}
+
+// 在本地文件系统上创造一个file_path对应的文件夹来保存对应的chunk， 并从buf中向文件中写入
+ssize_t ChunkStorage::write_chunk(const string &file_path, gaofs::rpc::chnk_id_t chunk_id, const char *buf, size_t size,
+                                  off64_t offset) const {
+    assert((offset+size) <= chunksize_);
+    // 初始化放chunk的文件夹，如果以及有了也无所谓
+    init_chunk_space(file_path);
+    // 获取全路径
+    auto chunk_path = absolute(get_chunk_path(file_path, chunk_id));
+
+    // 生成文件对应的file handle，并且用open函数打开，如果没有的话创建
+    FileHandle fh(open(chunk_path.c_str(), O_WRONLY | O_CREAT, 0640), chunk_path);
+    if(!fh.valid()) {
+        auto err_str = fmt::format(
+                "{}() Failed to open chunk file for write. File: '{}', Error: '{}'",
+                __func__, chunk_path, strerror(errno));
+        throw ChunkStorageException(errno, err_str);
+    }
+
+    size_t wrote_total{};
+    ssize_t wrote{};
+    do {
+        wrote = pwrite(fh.native(), buf + wrote_total, size - wrote_total, offset + wrote_total);
+
+        if(wrote < 0) {
+            if(errno == EINTR || errno == EAGAIN || errno == EWOULDBLOCK)
+                continue;
+            auto err_str = fmt::format(
+                    "{}() Failed to write chunk file. File: '{}', size: '{}', offset: '{}', Error: '{}'",
+                    __func__, chunk_path, size, offset, strerror(errno));
+            throw ChunkStorageException(errno, err_str);
+        }
+        wrote_total += wrote;
+    } while(wrote_total != size);
+
+    // 文件会关闭的，因为设置了析构函数，所以直接return
+    return wrote_total;
+}
+
+ssize_t ChunkStorage::read_chunk(const string &file_path, gaofs::rpc::chnk_id_t chunk_id, char *buf, size_t size,
+                                 off64_t offset) const {
+    assert(offset + size <= chunksize_);
+    auto chunk_path = absolute(get_chunk_path(file_path, chunk_id));
+
+    // 生成文件对应的file handle，并且用open函数打开
+    FileHandle fh(open(chunk_path.c_str(), O_RDONLY), chunk_path);
+    if(!fh.valid()) {
+        auto err_str = fmt::format(
+                "{}() Failed to open chunk file for read. File: '{}', Error: '{}'",
+                __func__, chunk_path, errno);
+        throw ChunkStorageException(errno, err_str);
+    }
+
+    size_t read = 0;
+    ssize_t read_total = 0;
+    do {
+        read = pread64(fh.native(), buf+read_total, size-read_total, offset+read_total);
+        if(read == 0) {
+            // read == 0 并不代表出错，可能是表示文件结束，所以不能认为出错了，直接break就好
+            break;
+        }
+
+        if(read < 0) {
+            if(errno == EINTR || errno == EAGAIN || errno == EWOULDBLOCK)
+                continue;
+            auto err_str = fmt::format(
+                    "{}() Failed to read chunk file. File: '{}', size: '{}', offset: '{}', Error: '{}'",
+                    __func__, chunk_path, size, offset, strerror(errno));
+            throw ChunkStorageException(errno, err_str);
+        }
+
+#ifndef NDEBUG
+        if(read + read_total < size) {
+            log_->debug("Read less bytes than requested: '{}'/{}. Total read was '{}'. This is not an error!",
+                        read, size - read_total, size);
+        }
+#endif
+        assert(read > 0);
+        read_total += read;
+    } while(read_total != size);
+
+    return read_total;
+}
+
+void ChunkStorage::trim_chunk_space(const string &file_path, gaofs::rpc::chnk_id_t chunk_start) {
+    auto chunk_dir = absolute(get_chunks_dir(file_path));
+    // 代表目录的结束
+    const fs::directory_iterator end;
+    auto err_flag = false;
+    for(fs::directory_iterator chunk_file(chunk_dir); chunk_file != end; chunk_file++) {
+        auto chunk_path = chunk_file->path();
+        auto chunk_id = std::stoul(chunk_path.filename().c_str());
+        if(chunk_id >= chunk_start) {
+            auto err = unlink(chunk_path.c_str());
+            // ENOENT = Error No Entry
+            if(err == -1 && errno != ENOENT) {
+                err_flag = true;
+                log_->warn(""
+                           "{}() Failed to remove chunk file. File: '{}', Error: '{}'",
+                           __func__, chunk_path.native(), strerror(errno));
+            }
+        }
+    }
+    if(err_flag) {
+        throw ChunkStorageException(EIO,fmt::format("{}() One or more errors occurred when truncating '{}'",__func__, file_path));
+    }
 }
 
 
